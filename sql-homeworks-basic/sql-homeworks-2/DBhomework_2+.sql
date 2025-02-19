4e9108169ef88fdaa711579a2098e57edf84737a
Alex-EDG <AlexEDG75@GMail.Com>
2025-02-19 21:50:18 +0300

    Initial commit

---
+++ sql-homeworks-basic/sql-homeworks-2/DBhomework_2+.sql
@@ -0,0 +1,13 @@
+-- один ко многим
+
+CREATE TABLE IF NOT EXISTS Employee (
+	Employee_id SERIAL PRIMARY KEY,
+	Employee_name VARCHAR(60) NOT NULL	
+);
+
+CREATE TABLE IF NOT EXISTS Department (
+	Department_id SERIAL PRIMARY KEY,
+	Employee_id INTEGER NOT NULL REFERENCES Employee(Employee_id),
+	Department_name VARCHAR(60) UNIQUE NOT NULL,
+	Department_head VARCHAR(60) NOT NULL
+);
